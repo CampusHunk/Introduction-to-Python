2b051581e5467009afa288542be2abc94e97ab2a
CampusHunk <nkosiczwe@gmail.com>
2024-11-04 21:08:20 +0200

    Add lesson one the print function

---
+++ Hello.py
@@ -0,0 +1,39 @@
+# **Let's say Hello**
+
+#Python, developed by Guido van Rossum in 1991, is loved by beginners and experts alike for its readable code and concise
+#syntax. This language is so versatile that it can power websites, analyze data, build machine learning models, automate tasks,
+#create games, and even control robots! 💻
+#Please pay close attention to the syntax here. Notice the keyword print, the parentheses, and the quotation marks. Got it?
+
+print("Hello World!")
+print('I am learning Python')
+
+
+# **Mind the indentation**
+
+#Even the simplest lines of code can trip you up with sneaky errors. 
+#So, we must learn about them to write better code.
+#One common mistake is extra indentation!
+
+#    print("Hello World!"
+#This won't work because of those extra spaces before print.)
+
+#In Python, think of print as a magic spell: you write the word print, and voila! ✨ Text appears on your screen.
+#Now, here's the cool part: you can use this spell over and over again, wherever you need it in your code. In Python language, we
+#call such a spell a function. So, when you see print() with those parentheses, it's like casting the spell to print something to
+#the screen. Cool, right? 😎
+#Each print() function will print on a new line.
+
+# **Can I give you a Nickname?**
+#Oops! Calling a function by the wrong name is another common mistake! 🤦‍♂️
+
+#pint("Hello, World!")
+#Looks like we’re ordering a drink instead of printing text! 🍺 It should be print, not pint.
+
+# **Mark the finish line please.**
+
+#Uh-oh! Looks like we forgot to close our string! 😬
+#print("Python) 
+#This won't work because it’s like starting a sentence and never finishing it.
+#Always make sure your strings are properly enclosed in quotes. 📝
+print("Coding is Fun!")
