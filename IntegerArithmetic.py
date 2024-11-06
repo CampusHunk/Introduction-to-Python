1eb50f375e50c72f184386e1fbc2468e4e4c4e2a
CampusHunk <nkosiczwe@gmail.com>
2024-11-06 22:16:32 +0200

    Add lesson four IntegerArithmetic

---
+++ IntegerArithmetic.py
@@ -0,0 +1,53 @@
+# **Introduction to integer arithmetic**
+
+#Arithmetic operations aren't just for math class — they're super important in programming too! Whether you're calculating
+# change at the store or figuring out the area of a room, arithmetic operations help you process numbers in programs.
+
+#Multiplication = *Finding the area of a rectangle from its length and width.
+#Addition = *Calculating the total expenses after shopping.
+#Subtraction = *Determining the remaining amount of milk after making a cup of coffee.
+#Division = *Calculating the average monthly revenue from the total yearly revenue.
+
+# **Basic arithmetic operations**
+
+#In the magical world of Python, you've got the power of basic arithmetic operations right at your fingertips! Think of it like
+# having a fully functional calculator built into your code. Need to add, subtract, multiply, or divide? Python's got your back!
+
+# **Addition and subtraction**
+
+#Alright, let's dive into addition and subtraction in Python! With the "+" symbol, you can add values effortlessly, while the "-"
+# symbol gracefully subtracts them. Some exaples:
+a = 5
+b = 3
+print(a + b)
+print(a - b, '\n')
+
+# **Checking balance**
+
+#Let's apply our new Python skill into practice. In the following code, we already defined two variables for you: total_money,
+# and cost. Please calculate the current balance by subtracting the cost from the total money and print the result.
+total_money = 100
+cost = 50
+print("The current balance is:",total_money - cost, 'Rands\n')
+
+# **Monthly savings**
+
+#Write a Python program that calculates your total savings over three months if you save 200 in the first month 150 in the 
+# second month, and 250 in the third month. Print the total savings.
+month_one = 150
+month_two = 200
+month_three = 250
+print(month_one + month_two + month_three, 'Rands\n')
+
+# **Multiplication and division**
+
+#Let's move forward to the next set of operations. We know you may have guessed the symbols for multiplication and division.
+# to be thorough, here are examples to illustrate multiplication and division:
+length = 12
+width = 6
+print(length * width)
+print(length / width, '\n')
+
+
+
+
