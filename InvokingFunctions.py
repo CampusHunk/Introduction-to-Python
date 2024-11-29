84703c045e8d12fc465e2e2dcf78994a8f46b2a3
CampusHunk <nkosiczwe@gmail.com>
2024-11-29 16:38:12 +0200

    Add more on string function

--- InvokingFunctions.py
+++ InvokingFunctions.py
@@ -7,7 +7,7 @@
 # become your favorite yet?
 
 #Here is a simple function call:
-multiply(2, 3)
+#multiply(2, 3)
 
 #Here multiply is the name of the function, and numbers in parentheses (1, 7) are its 
 # arguments. What is an argument? Well, it's just a value, that will be used inside the function 
@@ -25,8 +25,8 @@ multiply(2, 3)
 
 
 print("Hello, world!")
-print()
 print("Bye,","then")
+print()
 
 #So, the first call prints one string, and the second call of print without arguments prints, 
 # an empty line, and the last call outputs our two messages as one expression. Are you 
@@ -50,9 +50,9 @@ print("Bye,","then")
 # goes for any sequence, i.e. list, tuple, range, byte sequences, byte arrays).
 
 number = "111"
-
 # finding the leghth of an object
 print(len(number))
+print()
 
 #Then we declare the variables integer and float_number, convert our string to 
 # corresponding types, and write their sum into my_sum. By the way, the sum() function 
@@ -61,12 +61,9 @@ print(len(number))
 # coverting types
 integer = int(number)
 float_number = float(number)
-
 # adding numbers
 my_sum = sum((integer, float_number))
-
 #The result is a floating-point number, which becomes clear after printing my_sum.
-
 print(my_sum)
 print(round(my_sum))
 
@@ -74,26 +71,96 @@ print(round(my_sum))
 # the smallest number equals 3 and the largest number 8.4 (the sum of 3 and 5.4) belongs to floats.
 
 # finding the minimum and maximum.
-integer = 3
-float_number = 5.4
-my_sum = sum((integer, float_number))
-
-print(min(integer, float_number, my_sum))
-print(type( max(integer, float_number, my_sum)))
+num1 = 3
+num2 = 5.4
+my_sum = sum((num1, num2))
+print(min(num1, num2, my_sum))
+print(type( max(num1, num2, my_sum)))
 
 # **New function to sum numbers**
 
 #Create a new function to sum numbers.
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
+#Example
 def sum_numbers(a, b):
     return sum((a, b))
-    print("Result:", sum_numbers(num1, num2))
 
-print()
-x = int(input("Enter first number: "))
+x = int(input("\nEnter first number: "))
 y = int(input("Enter second number: "))
-print("Result:", sum_numbers(x, y))
 
+print("Result:", sum_numbers(x, y))
+print()
+
+#Example
+w = int(input("Enter first number: "))
+v = int(input("Enter second number: "))
+my_count = sum((w, v))
+print("Result of my count:", my_count)
+
+#Example
+c = int(input("\nEnter first number: "))
+u = int(input("Enter second number: "))
+print(sum((c, u)))
+print()
+
+# **Getting to know print() better**
+
+#Suppose we want to read the documentation for the function print.
+#We can do this by using the built-in function help().
+#help(print)
+
+#Young and beautiful
+# Read the input three times. Each one contains the age of a person – Jack's, Alex's, and Lana's. 
+# Find the youngest one among them and print this person's age.
+
+jacks_age = int(input("Enter Jack's age: "))
+alexs_age = int(input("Enter Alex's age: "))
+lanas_age = int(input("Enter Lana's age: "))
+print(min(jacks_age, alexs_age, lanas_age))
+print()
+
+# **min() and max() revisited**
+
+#Fun fact about min() and max() is that these functions can help you to sort in alphabetical order! If a string is passed as an 
+# argument, min() will return the first letter (alphabetically) and max() the last one (somewhat zetabetically). And if you 
+# specify more than one value, min() will return the first string in alphabetical order and max(), in contrast, the last one. 
+# For example, min("alpha", "omega") gives you "alpha", while max("alpha", "omega") returns "omega".
+
+print(max("gloomy", "grew", "green"))
+print(min("gloomy", "grey", "green"))
+print(min("green", "grass"))
+print(max("gloomy", "grey", "grass"))
+print()
+
+#Greeting a user with a function
+# In Python, we commonly use functions to execute a block of reusable code. You have a simple function to greet a user whose 
+# name is provided as an argument. Your goal is to invoke the function 'greet_user' with the argument 'name' storing a string 
+# 'Bob' and assign its return value to a variable 'message'. Later, the 'message' should be printed. 
+# Can you fill the blanks in the code to accomplish this task?
+
+def greet_user(name):
+    return "Hello, " + name
+    
+name = "Bob"
+message = greet_user(name)
+print(message)
+
+#Find the longest word
+# Create a program to find the longest of the words given and outputs the length of the longest word. 
+# Fill in the gaps with the correct built-in functions.
+
+word1 = "short"
+word2 = "long"
+word3 = "very long"
+def find_longest(word1, word2, word3):
+    return max(len(word1), len(word2), len(word3))
+
+print(find_longest(word1, word2, word3))
+print()
+
+#String length
+# Write a program that takes a string and prints its length.
+# The input string whose length you need to find is already stored inside the string variable, use it in your solution.
+
+my_string = input("Enter a name: ")
+print(len(my_string))
